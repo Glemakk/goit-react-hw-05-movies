ce99e50ad0ef1b6c7a55c6680a2bed44d5bb5960
Glemakk <kovgar.brand@gmail.com>
2021-07-29 19:44:44 +0200

    improved lazy.react & suspense with react spinner

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
+import { lazy, Suspense } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import AppBar from './components/AppBar'
-import HomeView from './views/HomeView'
-import MoviesView from './views/MoviesView'
-import MovieDetailsView from './views/MovieDetailsView'
-import NotFoundView from './views/NotFoundView'
+import Loader from './components/Loader'
+// import HomeView from './views/HomeView'
+// import MoviesView from './views/MoviesView'
+// import MovieDetailsView from './views/MovieDetailsView'
+// import NotFoundView from './views/NotFoundView'
+
+const HomeView = lazy(
+  async () =>
+    await import('./views/HomeView.js' /*webpackChunkName: "home-view"*/),
+)
+const MoviesView = lazy(
+  async () =>
+    await import('./views/MoviesView' /*webpackChunkName: "movies-view"*/),
+)
+const MovieDetailsView = lazy(
+  async () =>
+    await import(
+      './views/MovieDetailsView' /*webpackChunkName: "movie-details-view"*/
+    ),
+)
+const NotFoundView = lazy(
+  async () =>
+    await import('./views/NotFoundView' /*webpackChunkName: "not-found-view"*/),
+)
 
 const App = () => {
   return (
     <div>
       <AppBar />
-      <Switch>
-        <Route exact path="/">
-          <HomeView />
-        </Route>
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route path="/" exact>
+            <HomeView />
+          </Route>
 
-        <Route path="/movies" exact>
-          <MoviesView />
-        </Route>
+          <Route path="/movies" exact>
+            <MoviesView />
+          </Route>
 
-        <Route path="/movies/:movieId">
-          <MovieDetailsView />
-        </Route>
+          <Route path="/movies/:movieId">
+            <MovieDetailsView />
+          </Route>
 
-        <Route>
-          <NotFoundView />
-        </Route>
-      </Switch>
+          <Route>
+            <NotFoundView />
+          </Route>
+        </Switch>
+      </Suspense>
       <ToastContainer position="top-center" autoClose={2000} />
     </div>
   )
